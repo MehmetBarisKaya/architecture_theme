6b6a8a302db0fcce22f21fdd8b223d7662762710
Baris Kaya <baris.kaya99@hotmail.com>
2023-10-25 16:51:14 +0300

    Theme completed

---
+++ lib/src/theme/app_theme_light.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import 'app_theme.dart';
+import 'light/light_theme_interface.dart';
+
+class AppThemeLight extends AppTheme with ILightTheme {
+  AppThemeLight._init();
+  static AppThemeLight? _instance;
+  static AppThemeLight get instance {
+    _instance ??= AppThemeLight._init();
+    return _instance!;
+  }
+
+  static const _FONT_FAMILY = 'POPPINS';
+
+  @override
+  ThemeData get theme => ThemeData(
+        fontFamily: _FONT_FAMILY,
+        colorScheme: _appColorScheme,
+        textTheme: textTheme(),
+        appBarTheme: ThemeData.light().appBarTheme.copyWith(
+            color: Colors.transparent,
+            elevation: 0,
+            iconTheme: const IconThemeData(color: Colors.black87, size: 21)),
+        inputDecorationTheme: const InputDecorationTheme(
+            focusColor: Colors.black12,
+            labelStyle: TextStyle(),
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.zero,
+            filled: true,
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(width: 0.3)),
+            focusedBorder: OutlineInputBorder()),
+        scaffoldBackgroundColor: const Color(0xfff1f3f8),
+        floatingActionButtonTheme:
+            ThemeData.light().floatingActionButtonTheme.copyWith(),
+        buttonTheme: ThemeData.light().buttonTheme.copyWith(
+              colorScheme: const ColorScheme.light(
+                onError: Color(0xffFF2D55),
+              ),
+            ),
+        tabBarTheme: tabBarTheme,
+      );
+
+  TabBarTheme get tabBarTheme {
+    return TabBarTheme(
+      labelPadding: insets.lowPaddingAll,
+      labelColor: _appColorScheme.onSecondary,
+      labelStyle: textThemeLight.displaySmall,
+      unselectedLabelColor: _appColorScheme.onSecondary.withOpacity(0.2),
+      // unselectedLabelStyle: textThemeLight.headline4.copyWith(color: colorSchemeLight.red),
+    );
+  }
+
+  TextTheme textTheme() {
+    return ThemeData.light().textTheme.apply(fontFamily: _FONT_FAMILY);
+    // return TextTheme(
+    //   displayLarge: textThemeLight.displayLarge,
+    //   displayMedium: textThemeLight.displayMedium,
+    //   displaySmall: textThemeLight.displaySmall,
+    //   labelSmall: textThemeLight.overline,
+    // );
+  }
+
+  ColorScheme get _appColorScheme {
+    return ColorScheme(
+        primary: colorSchemeLight.black,
+        primaryContainer: Colors.white, //xx
+        secondary: Colors.green,
+        secondaryContainer: colorSchemeLight.azure,
+        surface: Colors.blue, //xx
+        background: const Color(0xfff6f9fc), //xx
+        error: const Color.fromARGB(255, 239, 7, 69),
+        onPrimary: Colors.greenAccent,
+        onSecondary: Colors.black, //x
+        onSurface: Colors.white30,
+        onBackground: Colors.black12,
+        onError: const Color(0xFFF9B916), //xx
+        brightness: Brightness.light);
+  }
+}
